419227bf5704a779f2853dfb279ab6391da1c31f
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-06-10 19:18:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "kubernetes" {
   host                   = "https://${module.gke.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-  # version                = "~> 2.10"
 }
 
 module "gke" {
