715de5aae9b4d10543e096e765a5920fecff1efa
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-06-10 20:52:22 +0530

    dfsa
    dfsasd

--- api/bucket.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "source" {
-  name          = var.bucket_name_source
-  storage_class = var.storage_class
-  location      = var.bucket_location
-  force_destroy = true
-}
