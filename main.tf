9e1c3fad5b5c597ba0f7a9c10208fa391bd50b50
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-06-10 14:10:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ module "gke" {
       name            = "node-pool"
       machine_type    = "e2-standard-2"
       node_locations  = "europe-west2-b,europe-west2-c"
-      min_count       = 2
-      max_count       = 5
+      min_count       = 1
+      max_count       = 2
       disk_size_gb    = 30
       spot            = false
       auto_upgrade    = true
