8660bb0a60ac4fd5f13d2b86cea374df3683d2ff
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-06-29 14:50:54 +0530

    compute.googleapis.com

--- api/variables.tf
+++ api/variables.tf
@@ -3,7 +3,7 @@
 variable "gcp_service_list" {
   type        = list(string)
   description = "The list of apis necessary for the project"
-  default     = []
+  default     = ["compute.googleapis.com"]
 }
 variable "project_id" {
   type        = string
