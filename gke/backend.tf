b20d8568e3b8e447ef309abffb2d0ec879d3fddc
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-07-16 21:07:40 +0530

    add the credentails

--- gke/backend.tf
+++ gke/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket = "gke-terraform-tfstate-bucket-demo-1" # GCS bucket name to store terraform tfstate
-    prefix = "gke-terraform-cluster"               # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
+    credentials = "terraform_credentails.json"
+    bucket      = "gke-terraform-tfstate-bucket-demo-1" # GCS bucket name to store terraform tfstate
+    prefix      = "gke-terraform-cluster"               # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
   }
 }
