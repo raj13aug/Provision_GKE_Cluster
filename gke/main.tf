13f075a84d50e98c5452fd81aba21acf05df293f
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2026-03-22 20:22:54 +0530

    Update main.tf

--- gke/main.tf
+++ gke/main.tf
@@ -95,8 +95,8 @@ module "gke" {
 }
 
 # # set up local kubectl client to access the newly created cluster
-# resource "null_resource" "configure-local-kubectl" {
-#   provisioner "local-exec" {
-#     command = "gcloud container clusters get-credentials ${module.gke.name} --region ${var.region} --project ${var.project_id}"
-#   }
-# }
+resource "null_resource" "configure-local-kubectl" {
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials ${module.gke.name} --region ${var.region} --project ${var.project_id}"
+  }
+}
